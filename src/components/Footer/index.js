0185bbe26346314488d0e1a1cff642f467fed5a4
AleexCaires <tiagoaleex98@gmail.com>
2021-01-13 14:56:41 +0000

    Footer Update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -169,7 +169,7 @@ const Footer = () => {
                             <SocialIconLink href="//www.twitter.com" target="_blank" aria-label="Twitter">
                                 <FaTwitter />
                             </SocialIconLink>
-                            <SocialIconLink href="/www.linkedin.com" target="_blank" aria-label="Linkedin">
+                            <SocialIconLink href="//www.linkedin.com" target="_blank" aria-label="Linkedin">
                                 <FaLinkedin />
                             </SocialIconLink>
                         </SocialIcons>
