ecf78f44172261040ed99bdcd83e37f9417e5034
AleexCaires <tiagoaleex98@gmail.com>
2021-01-13 14:55:11 +0000

    Chages to Data Section (Contact Us)

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,6 +1,6 @@
 import img1 from '../../images/svg-1.svg'
 import img2 from '../../images/svg-2.svg'
-import img3 from '../../images/svg-3.svg'
+import img3 from '../../images/svg-5.svg'
 
 
 export const homeObjOne = {
@@ -42,10 +42,10 @@ export const homeObjThree = {
     lightBg: true,
     lightText: false,
     lightTextDesc: false,
-    topLine: 'Premium Bank',
-    headline: 'Unlimited Transactions with zero fees',
-    description: 'Get access  to our exclusive app that allows you to send unlimited transactions ',
-    buttonLabel: 'Get started',
+    topLine: 'Contact Us',
+    headline: 'Unlimited Calls with zero fees',
+    description: 'Call us FREE (from a landline), for a FREE, No-Obligation quotation today ',
+    buttonLabel: 'Get Quote',
     imgStart: false,
     img: img3,
     alt: 'car',
